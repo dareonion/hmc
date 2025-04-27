ca8e07b169d143a2c9b44528340c1792c4523555
Darren Yin <darren.yin@gmail.com>
2025-04-26 21:05:05 -0700

    change

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-console.log('--- Loading astro.config.mjs ---'); // Add this line
-const base = '/hmc/'; // Or '/your-base'
-console.log(`--- Using base: ${base} ---`);   // Add this line
-
 export default defineConfig({
     site: 'https://dareonion.github.io',
-    base: base,
+    base: '/hmc/',
 });
